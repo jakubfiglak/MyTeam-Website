33a20783d48cb65dab1fef64fc56bee6b0c79288
Jakub Figlak <jakub.figlak@gmail.com>
2020-03-11 20:30:10 +0100

    Change social icons in directors section to svgs, add proper hover effects to directors section buttons

--- form.90d872a0.js
+++ form.90d872a0.js
@@ -137,7 +137,7 @@ inputs.forEach(function (input) {
     }
   });
 });
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../Users/jakub/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -165,7 +165,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54159" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56751" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -341,5 +341,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/form.js"], null)
+},{}]},{},["../../../Users/jakub/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/form.js"], null)
 //# sourceMappingURL=/form.90d872a0.js.map
